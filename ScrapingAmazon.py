751346fabec5d475aee33735023d0a1485b7ef31
Bilal Khan <lalbikhan2014@gmail.com>
2020-12-06 19:24:56 +0500

    Update ScrapingAmazon.py

--- ScrapingAmazon.py
+++ ScrapingAmazon.py
@@ -3,7 +3,7 @@ import requests
 from requests_html import HTMLSession
 session = HTMLSession()
 resp = session.get("https://www.amazon.com/Sceptre-E248W-19203R-Monitor-Speakers-Metallic/dp/B0773ZY26F/ref=sr_1_2?crid=1861TM8A5NDPX&dchild=1&keywords=monitors&qid=1597071906&sprefix=monitors%2Caps%2C364&sr=8-2")
-resp.html.render()
+resp.html.render(sleep=1, keep_page=True, timeout=20)
 soup = BeautifulSoup(resp.html.html, "lxml")
 title = soup.find(id = "productTitle").get_text().strip()
 print (title)
